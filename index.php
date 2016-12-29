2ede79e5bebee2c38a9ad446ded77a7d6ab652f3
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-12-29 14:55:56 +0700

    Updated style to be widescreen

--- index.php
+++ index.php
@@ -27,28 +27,33 @@ if($config['master']){
 	<script src="assets/PushStream.min.js"></script>
 </head>
 <body>
-<div id="control">
-	<button data-act="file">Open file</button>
-	<button data-act="youtube">Open YouTube</button>
-	<button data-act="announce">Set announce</button>
-</div>
-<div id="playerbox">
-	<video autoplay controls preload class="video-js" id="player">Connecting to message bus</video>
-	<div id="nicochatbox">
-		<div id="nicochat"></div>
+<div id="ui">
+	<div id="playerbox">
+		<video autoplay controls preload class="video-js" id="player">Connecting to message bus</video>
+		<div id="nicochatbox">
+			<div id="nicochat"></div>
+		</div>
 	</div>
-</div>
-<div id="chatbox">
-	<a class="user" target="_blank" href="https://www.facebook.com/app_scoped_user_id/<?=$user->id?>/">
-		<img src="<?=$user->avatar?>"> <?=htmlspecialchars($user->name)?>:
-	</a> <input type="text" name="chat" autofocus> 
-	<button id="nicotoggle">Nicochat</button> <button id="oztoggle" style="color:red;">OZView</button>
-	<span id="lagmeter">(<span id="lagdata">lag <span id="lag"></span>ms </span>ping <span id="lp"></span>ms)</span>
-	<ul id="chatmsg">
-		<li id="announce"></li>
-	</ul>
-	<ul id="onlineuser"></ul>
-	<div style="clear: both;"></div>
+	<div id="chatbox">
+		<div id="control">
+			<button data-act="file">Open file</button>
+			<button data-act="youtube">Open YouTube</button>
+			<button data-act="announce">Set announce</button>
+		</div>
+		<div id="inputrow">
+			<a class="user" target="_blank" href="https://www.facebook.com/app_scoped_user_id/<?=$user->id?>/">
+				<img src="<?=$user->avatar?>"> <?=htmlspecialchars($user->name)?>:
+			</a> <input type="text" name="chat" autofocus> 
+		</div>
+		<ul id="chatmsg">
+			<li id="announce"></li>
+		</ul>
+		<ul id="onlineuser"></ul>
+		<div id="settings">
+			<button id="nicotoggle">Nicochat</button> <button id="oztoggle" style="color:red;">OZView</button>
+			<span id="lagmeter">(<span id="lagdata">lag <span id="lag"></span>ms </span>ping <span id="lp"></span>ms)</span>
+		</div>
+	</div>
 </div>
 <div id="oz"></div>
 <script>window.HELP_IMPROVE_VIDEOJS = false;window.config = <?php echo json_encode($config); ?>;</script>
